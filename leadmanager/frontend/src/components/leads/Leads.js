96228afcf842626ee46becb0e5c25b5ec9f2ccfb
imon3 <imonovbude@gmail.com>
2019-09-26 20:14:49 -0700

    eight commit: created leads and types actions files, creted leads reducer file, made call to server to get leads in leads.js component

--- leadmanager/frontend/src/components/leads/Leads.js
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -1,13 +1,57 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Proptypes from 'prop-types';
+import { getLeads } from '../../actions/leads';
 
 export class Leads extends Component {
+  static propTypes = {
+    leads: Proptypes.array.isRequired
+  };
+
+  componentDidMount() {
+    this.props.getLeads();
+  }
+
   render() {
     return (
-      <div>
-        <h1>Leads List</h1>
-      </div>
+      <>
+        <h2>Leads List</h2>
+        <table className='table table-striped'>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Message</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.leads.map(lead => {
+              return (
+                <tr key={lead.id}>
+                  <td>{lead.id}</td>
+                  <td>{lead.name}</td>
+                  <td>{lead.email}</td>
+                  <td>{lead.message}</td>
+                  <td>
+                    <button className='btn btn-danger btn-sm'>Delete</button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </>
     );
   }
 }
 
-export default Leads;
+const mapStateToProps = state => ({
+  leads: state.leads.leads
+});
+
+export default connect(
+  mapStateToProps,
+  { getLeads }
+)(Leads);
