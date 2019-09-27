ce8f71ff1986d27573d7d8aa1da7fcb9e2879fcb
imon3 <imonovbude@gmail.com>
2019-09-26 18:14:18 -0700

    seventh commit: created leads form and dashboard components

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -2,10 +2,18 @@ import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 
 import Header from './layout/Header';
+import Dashboard from './leads/Dashboard';
 
 class App extends Component {
   render() {
-    return <Header />;
+    return (
+      <>
+        <Header />
+        <div className='container'>
+          <Dashboard />
+        </div>
+      </>
+    );
   }
 }
 
